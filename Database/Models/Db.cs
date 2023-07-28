3b05cc447177fd7af82d68892030b5b02e81adb2
Rodrigo <48519283+rodrigoescolante@users.noreply.github.com>
2023-07-28 14:08:07 -0300

    Add files via upload

---
+++ Database/Models/Db.cs
@@ -0,0 +1,14 @@
+﻿using CadastroFuncionarios.Classes;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace CadastroFuncionarios.Context
+{
+    public class Db : DbContext
+    {
+        public Db(DbContextOptions<Db> context) : base(context)
+        { }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+    }
+ }
